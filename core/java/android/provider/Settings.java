8046919b842fa522fc5a667850246dbb4b2f6a03
IDontCare-05 <Kristiantoadiwibowo84@gmail.com>
2021-09-06 08:36:58 +0300

    SystemUI: Add seekbar for Top/Left/Right statusbar Paddings [1/2]
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5795,6 +5795,24 @@ public final class Settings {
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
 
         /**
+         * Whether to add top padding on statusbar
+         * @hide
+         */
+        public static final String TOP_PADDING = "statusbar_top_padding";
+
+        /**
+         * Whether to add left padding on statusbar
+         * @hide
+         */
+        public static final String LEFT_PADDING = "statusbar_left_padding";
+
+        /**
+         * Whether to add right padding on statusbar
+         * @hide
+         */
+        public static final String RIGHT_PADDING = "statusbar_right_padding";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
