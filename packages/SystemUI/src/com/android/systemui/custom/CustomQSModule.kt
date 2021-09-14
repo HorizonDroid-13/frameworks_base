8de30aa892d01c91cba623687e40b6bf01336420
jhonboy121 <alfredmathew05@gmail.com>
2021-09-14 20:23:04 +0530

    base: added refresh rate tile
    * config_defaultPeakRefreshRate must be overlayed to enable this tile
    * supports switching between 60Hz (min) , peak refresh rate and auto mode
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@gmail.com>

--- packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
@@ -2,6 +2,8 @@ package com.android.systemui.custom
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.PowerShareTile
+import com.android.systemui.qs.tiles.RefreshRateTile
+
 
 import dagger.Binds
 import dagger.Module
@@ -15,4 +17,11 @@ interface CustomQSModule {
     @IntoMap
     @StringKey(PowerShareTile.TILE_SPEC)
     fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
+
+    /** Inject RefreshRateTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(RefreshRateTile.TILE_SPEC)
+    fun bindRefreshRateTile(refreshRateTile: RefreshRateTile): QSTileImpl<*>
+    
 }
